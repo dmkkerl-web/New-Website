519339e14667a3df5bbdc8d538898031cd4708a0
Dennis <dmk.kerl@gmail.com>
2026-05-26 20:49:47 +0300

    Added yet another  project

---
+++ mage-slider/index.js
@@ -0,0 +1,28 @@
+let right =document.getElementById("right");
+let left =document.getElementById("left");
+let container =document.querySelector(".container");
+let number =document.getElementById("number");
+
+function leftupdate(){
+
+    let maxwidth = container.scrollWidth -container.clientWidth;
+    container.scrollBy({
+        left :maxwidth/3,// or clientWidth
+        behavior:'smooth'
+    });
+
+   
+}   
+function rightupdate(){
+
+    let maxwidth = container.scrollWidth -container.clientWidth;
+    container.scrollBy({
+        left: -maxwidth/3,
+        behavior:'smooth'
+    });
+
+
+ }
+
+left.addEventListener('click',leftupdate);
+right.addEventListener('click',rightupdate);
