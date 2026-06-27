2673c5ef9d9bce8ef28c8033b89574cd1b557272
Dennis <dmk.kerl@gmail.com>
2026-06-27 13:16:13 +0300

    I added the menu icon for less clutter in the navbar

--- index.js
+++ index.js
@@ -8,3 +8,32 @@ function clockprogram(){
 }
 clockprogram();
 setInterval(clockprogram,1000);
+
+//sidebar logic
+
+let sidebar=document.getElementById("side-bar");
+let Button=document.getElementById("menu-toggle");
+let Showing=false;
+function DisplaySideBar(){
+    console.log("diplaying SideBar");
+    sidebar.style.transform="translateX(0)";
+}
+Button.addEventListener('click',()=>{
+    if (!Showing){
+    Button.innerHTML="&#10005;"
+    DisplaySideBar();
+    Showing=true;
+    
+    }
+    else{
+        Button.innerHTML="&#9776;"
+        CloseSideBar();
+        Showing=false;
+        
+    }
+})
+
+function CloseSideBar(){
+    console.log("hiding sidebar")
+    sidebar.style.transform="translateX(-120px)";
+}
